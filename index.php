9a4142eea900d21859a33d5c22d4ecc3a54972ea
DavideGila <davide.gila@gmail.com>
2023-11-27 14:33:00 +0100

    scaffholding

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+   <form action="censura.php" method="GET">
+    <textarea name="paragraph" cols="30" rows="10"></textarea>
+   </form>
+    
+</body>
+</html>
